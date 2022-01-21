b04849e24cd64b57a6075b0522406eeccd575db9
DeniisJuu98 <76124404+DeniisJuu98@users.noreply.github.com>
2022-01-21 09:46:44 +0100

    Update Controlador2.java

--- BS2_Inyeccioón_de_dependencias/Controlador2.java
+++ BS2_Inyeccioón_de_dependencias/Controlador2.java
@@ -6,14 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
 public class Controlador2 {
-
+//Importamos la dependencia de la clase servicio
     @Autowired
     Servicio servicio;
 
+    //Usamos el post para devolver los datos una vez editados
     @PostMapping("/persona2/getPersona")
     public Persona getPersonaNueva (){
+        //creamos ua nueva persona, a la que le introducimos la persona creada en el controlador 1.
         Persona p = (Persona) servicio.getPersona();
+        //Cogemos la edad y la multiplicamos por 2.
         p.setEdad(p.getEdad()*2);
+        //Devolvemos persona
         return p;
     }
 
