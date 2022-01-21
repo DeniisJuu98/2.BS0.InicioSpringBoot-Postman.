e8bc030c207917a4353e140a73c6acc921d1c028
DeniisJuu98 <76124404+DeniisJuu98@users.noreply.github.com>
2022-01-21 09:39:53 +0100

    Update Controlador1.java

--- BS0_Iniciacion_SpringBoot/Controlador1.java
+++ BS0_Iniciacion_SpringBoot/Controlador1.java
@@ -2,16 +2,15 @@ package BS0_Iniciacion_SpringBoot;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+//Definimos la clase como controlador
 @RestController()
 public class Controlador1 {
 
-    @Autowired
-    Herramienta herramienta;//llama a HerramientImpl para que se declare una sola vez
-
+    //Clase para hola mundo
+    //Usamos @GetMappig para devolver datos.
     @GetMapping("/a")
     public String getHola(){
-
+        //devolvemos el "Hola mundo"
         return "Hola Mundo";
     }
     @GetMapping("/b")
@@ -20,16 +19,14 @@ public class Controlador1 {
         return "Hola MundoBBBB";
     }
 
+    //Devolvemos una frase con la id introducida por los parametros
     @GetMapping("/nombre/{id}")
         public String getNombreId(@PathVariable String id){
             return "Id del usuario :"+ id;
         }
 
-    @GetMapping("/herramient")
-    public String dameHerramienta(@PathVariable String herrmaient){
-        return herramienta.getHerramienta(herrmaient);
-    }
 
+    //Buscamos los datos de los usuarios y les sumamos 1 a su edad
     @PostMapping("/usuario")
     public Usuario getUsuario(@RequestBody Usuario usu){
         usu.setEdad(usu.getEdad()+1);
