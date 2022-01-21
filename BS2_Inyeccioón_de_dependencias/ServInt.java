f72ef055b98a924fd5e59878f758f80ff36fc188
DeniisJuu98 <76124404+DeniisJuu98@users.noreply.github.com>
2022-01-21 09:48:06 +0100

    Update ServInt.java

--- BS2_Inyeccioón_de_dependencias/ServInt.java
+++ BS2_Inyeccioón_de_dependencias/ServInt.java
@@ -1,6 +1,7 @@
 package BS2_Inyeccioón_de_dependencias;
-
+//usamos una interfaz para la clase servicio
 public interface ServInt {
 
+    //este metodo creara un nuevo objeto persona
     Persona createPersona(String nombre, int edad, String provincia);
 }
