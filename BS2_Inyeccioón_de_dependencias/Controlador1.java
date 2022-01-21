25922945d7311f7eac0cea4530746b0c8730bd75
DeniisJuu98 <76124404+DeniisJuu98@users.noreply.github.com>
2022-01-21 09:42:31 +0100

    Update Controlador1.java

--- BS2_Inyeccioón_de_dependencias/Controlador1.java
+++ BS2_Inyeccioón_de_dependencias/Controlador1.java
@@ -6,15 +6,18 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
+//Definimos como controlador
 @RestController()
 public class Controlador1 {
-
+//Introducimos la dependencia de la clase servicio
     @Autowired
     Servicio servicio;
 
+    //Cogemos los datos introducidos en el Header de Postman, para crear un objeto Persona
+    
     @GetMapping("/controlador1/addPersona")
     public Persona getPersona (@RequestHeader(value = "nombre") String nombre,@RequestHeader(value = "edad") int edad,@RequestHeader(value = "provincia") String provincia){
+        //Para la creacion de persona, usamos el metodo imporado de servicio, y usamos su clase creada anteriormente
         return servicio.createPersona(nombre,edad,provincia);
     }
 
