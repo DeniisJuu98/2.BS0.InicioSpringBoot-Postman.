441599ed7b29613d87e40e0588fd724561bd2814
DeniisJuu98 <76124404+DeniisJuu98@users.noreply.github.com>
2022-01-21 09:40:42 +0100

    Update Usuario.java

--- BS0_Iniciacion_SpringBoot/Usuario.java
+++ BS0_Iniciacion_SpringBoot/Usuario.java
@@ -2,6 +2,8 @@ package BS0_Iniciacion_SpringBoot;
 
 import lombok.Data;
 
+//Creamos una clase simple de usuario, para ser luego buscada en un JSON
+//Con lo que tienen que ser los mismos parametros del objeto
 @Data
 public class Usuario{
     String nombre;
