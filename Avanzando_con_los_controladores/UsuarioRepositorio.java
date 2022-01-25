4781709dd8e0121792d44fc7c69a661655e3aa6b
DeniisJuu98 <76124404+DeniisJuu98@users.noreply.github.com>
2022-01-25 15:59:57 +0100

    Update UsuarioRepositorio.java

--- Avanzando_con_los_controladores/UsuarioRepositorio.java
+++ Avanzando_con_los_controladores/UsuarioRepositorio.java
@@ -1,7 +1,8 @@
-package RS1.Avanzando_con_los_controladores;
+package RS1_Avanzando_con_los_controladores;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface UsuarioRepositorio extends JpaRepository<com.example.demo1.Usuarios,Integer>{
+public interface UsuarioRepositorio extends JpaRepository<Usuarios,Integer>{
 }
