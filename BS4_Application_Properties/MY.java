be5d7dac2b1afa649787d8e56e66b466b5630246
DeniisJuu98 <76124404+DeniisJuu98@users.noreply.github.com>
2022-01-27 11:04:38 +0100

    Add files via upload

---
+++ BS4_Application_Properties/MY.java
@@ -0,0 +1,18 @@
+package BS4_Application_Properties;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "my")
+public class MY {
+    private String VAR2;
+
+    public String getVAR2() {
+        return VAR2;
+    }
+
+    public void setVAR2(String VAR2) {
+        this.VAR2 = VAR2;
+    }
+
+}
